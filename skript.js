2304f8ed4fdb909aa0a27dc560cb01d3b445e3d3
Sussygenss <165513316+Anthprony@users.noreply.github.com>
2026-04-02 18:34:20 +0100

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,1 +1,6 @@
-
+// Nothing fancy needed for this, but we can log clicks if you want
+document.querySelectorAll('.btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log('Link clicked:', btn.previousElementSibling.textContent);
+    });
+});
